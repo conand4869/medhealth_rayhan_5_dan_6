ce18489d0fff252a2d65ec9999d2b8d5e70425e6
Muhammad Rayhan R <78771007+conand4869@users.noreply.github.com>
2021-06-16 21:20:55 +0700

    Add files via upload

---
+++ medhealth_db/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "medhealth";
+    $connection = mysqli_connect($host, $username, $password, $db_name);
+
+    if (!$connection) {
+        # code...
+        echo "Connection Failed";
+    }
+?>
